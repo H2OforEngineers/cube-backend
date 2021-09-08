265dd36de5b32dc35c7c622046a25d3304850930
Yazan Baker <yazanbaker94@hotmail.com>
2021-09-08 15:08:47 +0300

    done

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -3,9 +3,20 @@
 require('dotenv').config();
 const { Sequelize, DataTypes } = require('sequelize');
 
-const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/cube';
+const DATABASE_URL = process.env.DATABASE_URL;
 
-let sequelize = new Sequelize(DATABASE_URL);
+let sequelizeOptions = {
+  dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    }
+};
+
+let sequelize = new Sequelize(DATABASE_URL, sequelizeOptions);
+
+
 
 const mechanicModel = require('./mechanic.model');
 const electricModel = require('./electric.model');
