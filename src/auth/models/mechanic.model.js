b922b2ae9e7e8dc5116710a803cc39f66f5ce309
siltan-elayan <sultan.m.elayan@gmail.com>
2021-10-02 15:30:40 +0300

    fixing

--- src/auth/models/mechanic.model.js
+++ src/auth/models/mechanic.model.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const mechanicModel = (sequelize, DataTypes) => sequelize.define('mechanic', {
-  title: { type: DataTypes.STRING, required: true },
-  image: { type: DataTypes.STRING, required: true },
-  description: { type: DataTypes.STRING, required: true },
-  phoneNumber: { type: DataTypes.STRING},
+  name: { type: DataTypes.STRING, required: true },
+  category: { type: DataTypes.STRING, required: true },
+  description: { type: DataTypes.STRING },
+  inStock: { type: DataTypes.INTEGER},
+  image: { type: DataTypes.BLOB},
+  phoneNumber: { type: DataTypes.STRING, required: true },
+  price: { type: DataTypes.STRING},
+
 });
 
 module.exports = mechanicModel;
